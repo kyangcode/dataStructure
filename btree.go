a8c6c9e90e5b523238f231ac425970c51a2b449a
kyangcode <kyangm@163.com>
2024-01-19 22:42:19 +0800

    feat: B树完善

--- btree.go
+++ btree.go
@@ -5,6 +5,16 @@ import (
 	"sort"
 )
 
+// B树的定义
+// 对于m阶的B树，需要满足以下条件：
+// 1. 叶子节点在同一层
+// 2. 除叶子节点，其他节点最多有m-1个关键字
+// 3. 除叶子节点，其他节点最少有ceil(m/2)-1个关键字，根节点最少有1个关键字
+// 4. 除叶子节点，若有n个关键字，则有n+1棵子树
+// 5. 节点中关键字非降序排列
+// 6. 假设y为某个节点关键字的左子树，则以y为根的子树上关键字都不大于当前节点关键字。
+// 7. 假设z为某个节点关键字的右子树，则以z为根的子树上关键字都不小于当前节点关键字。
+
 type Data interface {
 	Less(data Data) bool
 }
@@ -39,6 +49,13 @@ func NewBTree(m int) *BTree {
 	}
 }
 
+// Insert 插入
+// 假设最大关键字数为max
+// 1. 先查找一个合适的叶子节点，并将关 键字插入
+// 2. 若叶子节点的关键字数大于max，则需要分裂
+// 3. 按照中间的位置，将当前节点分裂成两个节点
+// 4. 将关键关键字插入到父亲节点
+// 5. 父节点递归调用分裂的函数
 func (t *BTree) Insert(data Data) {
 	if t.Root == nil {
 		t.Root = &Node{}
@@ -74,13 +91,13 @@ func (t *BTree) Delete(data Data) {
 
 	index, isExist := t.Root.List.BinarySearch(data)
 	if isExist {
-		if len(t.Root.Children) == 0 {
+		if len(t.Root.Children) == 0 { // 叶子节点，直接删除对应元素
 			if index == len(t.Root.List)-1 {
 				t.Root.List = t.Root.List[0:index]
 			} else {
 				t.Root.List = append(t.Root.List[0:index], t.Root.List[index+1:]...)
 			}
-		} else {
+		} else { // 内部节点
 			left := t.Root.Children[index]
 			right := t.Root.Children[index+1]
 			if len(left.List) >= min {
@@ -102,6 +119,9 @@ func (t *BTree) Delete(data Data) {
 			} else {
 				left.List = append(left.List, append(List{data}, right.List...)...)
 				left.Children = append(left.Children, right.Children...)
+				for _, child := range right.Children {
+					child.Parent = left
+				}
 
 				if index == len(t.Root.List)-1 {
 					t.Root.List = t.Root.List[0:index]
@@ -112,6 +132,14 @@ func (t *BTree) Delete(data Data) {
 					t.Root.Children = t.Root.Children[0 : index-1]
 				} else {
 					t.Root.Children = append(t.Root.Children[0:index], t.Root.Children[index+2:]...)
+				}
+
+				if len(t.Root.List) == 0 {
+					t.Root.List = left.List
+					t.Root.Children = left.Children
+					for _, child := range left.Children {
+						child.Parent = t.Root
+					}
 				}
 
 				newTree := &BTree{
@@ -142,16 +170,21 @@ func (t *BTree) Delete(data Data) {
 				node.List = append(List{t.Root.List[index-1]}, node.List...)
 				t.Root.List[index-1] = lefBrother.List[len(lefBrother.List)-1]
 				node.Children = append([]*Node{lefBrother.Children[len(lefBrother.Children)-1]}, node.Children...)
+				lefBrother.Children = lefBrother.Children[0 : len(lefBrother.Children)-1]
 			} else if rightBrother != nil && len(rightBrother.List) >= min {
 				node.List = append(node.List, t.Root.List[index])
 				t.Root.List[index] = rightBrother.List[0]
 				node.Children = append(node.Children, rightBrother.Children[0])
+				rightBrother.Children = rightBrother.Children[1:]
 			} else {
 				if lefBrother != nil {
 					node.List = append(lefBrother.List, append(List{t.Root.List[index-1]}, node.List...)...)
 					t.Root.List = append(t.Root.List[0:index-1], t.Root.List[index:]...)
 
 					node.Children = append(lefBrother.Children, node.Children...)
+					for _, child := range lefBrother.Children {
+						child.Parent = node
+					}
 					t.Root.Children = append(t.Root.Children[0:index-1], t.Root.Children[index:]...)
 				} else if rightBrother != nil {
 					node.List = append(node.List, append(List{t.Root.List[index]}, rightBrother.List...)...)
@@ -162,10 +195,21 @@ func (t *BTree) Delete(data Data) {
 					}
 
 					node.Children = append(node.Children, rightBrother.Children...)
+					for _, child := range rightBrother.Children {
+						child.Parent = node
+					}
 					if index+1 == len(t.Root.Children) {
 						t.Root.Children = t.Root.Children[0 : index+1]
 					} else {
 						t.Root.Children = append(t.Root.Children[0:index+1], t.Root.Children[index+2:]...)
+					}
+				}
+
+				if len(t.Root.List) == 0 {
+					t.Root.List = node.List
+					t.Root.Children = node.Children
+					for _, child := range node.Children {
+						child.Parent = t.Root
 					}
 				}
 			}
--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "strconv"
 
-//type User struct {
-//	ID   int
-//	Name string
-//}
-//
-//func (u *User) Less(data Data) bool {
-//	return u.ID < data.(*User).ID
-//}
+type User struct {
+	ID   int
+	Name string
+}
+
+func (u *User) Less(data Data) bool {
+	return u.ID < data.(*User).ID
+}
 
 func main() {
-	//tree := NewBTree(3)
-	//
-	//for i := 1; i <= 100; i++ {
-	//	tree.Insert(&User{
-	//		ID:   i,
-	//		Name: "n" + strconv.Itoa(i),
-	//	})
-	//}
+	tree := NewBTree(3)
 
-	s := []int{1, 2, 3}
-	r := sort.Search(3, func(i int) bool {
-		return 0 < s[i]
-	})
-	fmt.Println(r)
+	for i := 1; i <= 5; i++ {
+		tree.Insert(&User{
+			ID:   i,
+			Name: "n" + strconv.Itoa(i),
+		})
+	}
+
+	for i := 5; i >= 1; i-- {
+		tree.Delete(&User{
+			ID: i,
+		})
+	}
+
 }
